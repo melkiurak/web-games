951c67d26957649b68cf1e7781e2d59230c4d3e4
Danila <melkiurak3209@gmail.com>
2025-04-03 20:52:20 +0300

    Adaptive

--- src/Components/GameFilter/GameFilter.jsx
+++ src/Components/GameFilter/GameFilter.jsx
@@ -9,12 +9,17 @@ export const GameFilter = ({dataGames, dataDiscounts}) => {
     const [gamesCount, setGamesCount] = useState(10);
     const [discountsPrice, setDiscountsPrice] = useState(null);
     const handleGameCount = () => {
-        if(window.innerWidth <= 768){
-            setGamesCount(4);
-        } else{
-            setGamesCount(10);
-        };
-    }; 
+        if (window.innerWidth <= 541) {
+            setGamesCount(4); 
+        }else if (window.innerWidth <= 767) {
+            setGamesCount(6); 
+        }
+         else if (window.innerWidth <= 1023) {
+            setGamesCount(8);
+        } else {
+            setGamesCount(10); 
+        }
+    };
     const discount = useCallback((currentSlideGames) => {
         if (!currentSlideGames || !dataDiscounts?.discounts?.edges?.length) {
             return [];
@@ -50,36 +55,41 @@ export const GameFilter = ({dataGames, dataDiscounts}) => {
     }, [dataChek]);
     useEffect(() => {
         handleGameCount();
-        window.addEventListener('resize', handleGameCount);
-
-        return () => {
-            window.removeEventListener('resize', handleGameCount);
+    
+        const handleResize = () => {
+            handleGameCount();
         };
-    }, [])
-    return <div>
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    return <div className="max-lg:px-5 max-md:px-3">
         <div className="container flex flex-col gap-8">
             <div className="flex flex-col text-center">
                 <h2 className="text-white">Games By Filter</h2>
                 <p className="text-[#979797]">At This Section You Can Search For Games by multiple filters</p>
             </div>
             <Filters dataGames={dataGames} setResultSearch={setResultSearch}/>
-            <div className="grid grid-cols-5 justify-items-center gap-y-10">
+            <div className="grid grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-small-screen:!grid-cols-2 justify-items-center gap-y-10 max-desktop:gap-y-5 max-lg:gap-y-5 gap-x-4 max-desktop:gap-x-3 max-lg:gap-x-4">
                 {((resultSearch?.length > 0 ? resultSearch : dataGames?.games?.edges || []).slice(0, gamesCount).map((game, index) => 
-                    <div key={game.objectId || game.node?.objectId} className="w-[227.2px] p-[10px] border border-[#7D3C98] rounded-lg flex flex-col gap-3">
-                        <div className="w-full h-[239px] max-md:h-[194px] bg-center max-md:bg-[center_top_-3rem] max-phone:!bg-center bg-cover bg-no-repeat rounded-lg" style={{backgroundImage: `url('${game.BannerImg?.url || game.node?.BannerImg?.url}')`}}></div>
+                    <div key={game.objectId || game.node?.objectId} className="w-full p-[10px] max-desktop:p-2 border border-[#7D3C98] rounded-lg flex flex-col gap-3">
+                        <div className="w-full h-[239px] max-desktop:h-[178px] max-lg:h-[200px] max-md:h-[250px] max-small-screen:!h-[200px] bg-center  bg-cover bg-no-repeat rounded-lg" style={{backgroundImage: `url('${game.BannerImg?.url || game.node?.BannerImg?.url}')`}}></div>
                         <div className="flex flex-col gap-3 ">
-                            <p className="text-white font-medium">{game.name || game.node?.name}</p>
+                            <p className="text-white font-medium truncate">{game.name || game.node?.name}</p>
                             <div className="flex justify-between">
                                 <div className="flex items-center gap-1">
-                                    <MdOutlineCalendarMonth className="text-white text-xl"/>
-                                    <span className="text-[#979797] text-xs font-light">{new Date(game.date || game.node?.date).toLocaleDateString("en-US", {
+                                    <MdOutlineCalendarMonth className="text-white text-xl max-md:text-base"/>
+                                    <span className="text-[#979797] text-xs max-md:text-[10xp] font-light">{new Date(game.date || game.node?.date).toLocaleDateString("en-US", {
                                         year: 'numeric',
                                     })}</span>
                                 </div>
                                 <div className="max-w-[97.6px] max-desktop:max-w-[75.6px] w-full h-full flex gap-1 items-center ">
-                                    <img className="w-5 h-5" src={metacritic} alt="" />
-                                    <span className="text-[#FFCC00] text-base font-medium">{game.metacriticScore || game.node?.metacriticScore}</span>  
-                                    <span className="text-[#979797] text-sm">/{game.metacriticScoreMax || game.node?.metacriticScoreMax}</span>
+                                    <img className="w-5 max-md:w-4 h-5 max-md:h-4" src={metacritic} alt="" />
+                                    <span className="text-[#FFCC00] text-base max-md:text-xs font-medium">{game.metacriticScore || game.node?.metacriticScore}</span>  
+                                    <span className="text-[#979797] text-sm max-md:text-[10px]">/{game.metacriticScoreMax || game.node?.metacriticScoreMax}</span>
                                 </div>
                             </div>
                             <div className="flex justify-between max-desktop:flex-col max-desktop:items-center max-desktop:gap-2">
