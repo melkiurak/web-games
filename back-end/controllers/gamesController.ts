f4612949ff2739ceebbb35773717db84b55a1a6c
Danila <melkiurak3209@gmail.com>
2026-03-06 12:28:41 +0200

    Add verification the server

--- back-end/controllers/gamesController.ts
+++ back-end/controllers/gamesController.ts
@@ -1,5 +1,16 @@
+
 export const createGame = (req:any, res:any) => {
     console.log("Запрос пришёл")
     console.log(req.body)
-    res.send('Game Created')
+    if(!req.body.name) {
+        res.status(400).json({
+            error: 'Name is required'
+            
+        })
+        return;
+    } 
+    res.status(201).json({
+        message: 'Game Created'
+    });
+
 }
