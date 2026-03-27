b4f5643bdadda98ef9e57e32c7cdc77a49246643
Danila <melkiurak3209@gmail.com>
2026-03-27 14:30:20 +0200

    Day 5

--- front-end/src/hooks/useFilter.ts
+++ front-end/src/hooks/useFilter.ts
@@ -1,20 +1,48 @@
 import { GameFilter } from "@/types";
+import { split } from "@apollo/client";
+import { platform } from "node:os";
 import { useCallback, useMemo, useState } from "react"
+import { useSearchParams } from "react-router";
 
 export const useFilter = <T extends {genres: any[], platforms: any[]}> (allGame: T[]) => {
-    const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
-    const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
+   // const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+    //const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    
+    
+    const selectedGenres = useMemo(() => {
+        return searchParams.get('genre')?.split(',') || []
+    }, [searchParams]);
+   
+    const selectedPlatforms = useMemo(() => {
+        return searchParams.get('platform')?.split(',') || []
+    }, [searchParams])
     
     const toggleGenre = useCallback((genre: string) => {
-        setSelectedGenres(prev => prev.includes(genre) ? prev.filter(g => g !== genre) : [...prev, genre]);
-    }, []);
+        const params = new URLSearchParams(searchParams);
+        const updated = selectedGenres.includes(genre) ? selectedGenres.filter(g => g !== genre) : [...selectedGenres, genre];
+        if(updated.length > 0){
+            params.set('genre', updated.join(','))
+        } else {
+            params.delete('genre')
+        }
+        setSearchParams(params)
+    }, [selectedGenres, searchParams, setSearchParams]);
 
     const togglePlatform = useCallback((platform: string) => {
-        setSelectedPlatforms(prev => prev.includes(platform) ? prev.filter(p => p !== platform) : [...prev, platform]);
-    }, []);
+        const params = new URLSearchParams(searchParams);
+        const updated = selectedPlatforms.includes(platform) ? selectedPlatforms.filter(p => p !== platform) : [...selectedPlatforms, platform];
+        if(updated.length > 0) {
+            params.set('platform', updated.join(','))            
+        } else {
+            params.delete('platform')
+        }
+        setSearchParams(params);
+    }, [selectedPlatforms, searchParams, setSearchParams]);
 
     const filterGames = useMemo(() => {
         let result = [...allGame];
+        
         if (selectedGenres.length > 0) {
             const lowerSelected = selectedGenres.map(s => s.toLowerCase());
             result = result.filter(game => 
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.js'
 import { ApolloProvider } from '@apollo/client'
 import client from './Service/gameServer'
+import { BrowserRouter } from 'react-router'
 const element = document.getElementById('root') as HTMLElement
 
 createRoot(element).render(
   <StrictMode >
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
