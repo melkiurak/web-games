4417dd2ba68ccc47733b02a010734e8fb733561d
Danila <melkiurak3209@gmail.com>
2026-05-26 15:59:16 +0300

    Validate a Page

---
+++ back-end/src/middlewares/validatePage.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+export const validatePage = ( req:Request, res:Response, next:NextFunction ) => {
+    const page = Number(req.query.page)
+    
+    if(isNaN(page) || page < 1 ) {
+        res.status(400).json({
+            success: false,
+            message: 'Page Not Validate',
+        })
+        return
+    }
+    next();
+
+}
--- back-end/src/routes/game.routes.ts
+++ back-end/src/routes/game.routes.ts
@@ -3,6 +3,7 @@ import { GameController } from "../controllers/game.controller.js";
 
 const gameRouter = Router();
 const gameController = new GameController();
+
 gameRouter.get('/game', gameController.getGames)
 gameRouter.get('/metadata', gameController.getMetadata);
 
